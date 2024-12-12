1dd15f83a56359be33485297f6c1eb6671aa0ba9
sreehari k <162795824+sreeh1234@users.noreply.github.com>
2024-12-12 18:05:53 +0530

    api work

---
+++ rst_frmwrk/app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    roll_no=models.IntegerField()
+    name=models.CharField(max_length=20)
+    age=models.IntegerField()
+    email=models.EmailField()
