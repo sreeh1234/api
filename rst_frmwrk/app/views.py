a86fc5e033569e0c00663c51e756b02d3e3962ff
sreehari k <162795824+sreeh1234@users.noreply.github.com>
2024-12-16 17:47:16 +0530

    api work

--- rst_frmwrk/app/views.py
+++ rst_frmwrk/app/views.py
@@ -7,6 +7,8 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import generics,mixins
 
 
 # Create your views here.
@@ -92,6 +94,71 @@ def fun6(req,d):
     elif req.method=='DELETE':
         demo.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class fun7(APIView):
+    def get(self,req):
+        demo=Student.objects.all()
+        s=Model_serializer(demo,many=True)
+        return Response(s.data)
+    def post(self,req):
+        s=Model_serializer(data=req.data)
+        if s.is_valid():
+            s.save()
+            return JsonResponse(s.data,status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse(s.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+class fun8(APIView):
+    def get(self,req,d):
+        try:
+            demo = Student.objects.get(pk=d)
+            s = Model_serializer(demo)
+            return Response(s.data)
+        except Student.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    
+    def put(self,req,d):
+        try:
+            demo = Student.objects.get(pk=d)
+            s = Model_serializer(demo,data=req.data)
+            if s.is_valid():
+                s.save()
+                return Response(s.data)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)      
+        except Student.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)      
+    
+    def delete(self,req,d):
+        try:
+            demo = Student.objects.get(pk=d)
+            demo.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)      
+        except Student.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND) 
+
+
+class genericapiview(generics.GenericAPIView,mixins.ListModelMixin,mixins.CreateModelMixin):
+    serializer_class=Model_serializer
+    queryset=Student.objects.all()
+    def get(self,req):
+        return self.list(req)
+    def post(self,req):
+        return self.create(req)
+    
+
+class update(generics.GenericAPIView,mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin):  
+    serializer_class=Model_serializer
+    queryset=Student.objects.all()
+    lookup_field = 'id'
+    def get(self,req,id=None):
+        return self.retrieve(req)
+    def put(self,req,id=None):
+        return self.update(req,id)
+    def delete(self,req,id=None):
+        return self.destroy(req,id)
+
 
 
         
--- rst_frmwrk/rst_frmwrk/urls.py
+++ rst_frmwrk/rst_frmwrk/urls.py
@@ -26,4 +26,7 @@ urlpatterns = [
     path('fun4/<d>',views.fun4),
     path('fun5',views.fun5),
     path('fun6/<d>',views.fun6),
+    path('fun7',views.fun7.as_view()),
+    path('fun8/<d>',views.fun8.as_view()),
+    path('fun9',views.genericapiview.as_view()),
 ]
